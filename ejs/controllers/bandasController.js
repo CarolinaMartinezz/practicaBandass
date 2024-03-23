35e2d1126f147c9a27703598df4ca0baed986d1f
CarolinaMartinezz <cbmartinez@udesa.edul.ar>
2024-03-23 10:26:07 -0300

    Creacion de funciones en los controladores

--- ejs/controllers/bandasController.js
+++ ejs/controllers/bandasController.js
@@ -5,9 +5,21 @@ const bandasController = {
         return res.send(db.lista)
     },
     id: function (req,res){
+        let id = req.params.id;
+        for(let i=0; i<db.lista.length; i++){
+            if (id == db.lista[i].id){
+                return res.send(db.lista[i])
+            } else if (id > db.lista.length) {
+                return res.send("No encontramos a la banca indicada. Por favor, elija otro id.")
+            }
+        }
 
     },
     generos:function (req,res){
-
+        let resultado = [];
+        for (let i=0; i < db.lista.length; i++){
+            resultado.push(db.lista[i].genero);
+        }
+        return res.send(resultado)
     }
 }
